15ab84447012da8afa977af762eaa07102a6d9c1
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2026-08-21 12:50:14 -0500

    created components and added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
+import Auth from "./pages/Auth";
+import Checkout from "./pages/Checkout";
 
 function App() {
-  return <div></div>;
+  return (
+    <div className="app">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/checkout" element={<Checkout />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
