5c316fe8262b1116ac6c87a5623436ce00aacc86
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2026-08-21 13:12:25 -0500

    created product grid on home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,7 @@
+import { Link } from "react-router-dom";
+import { getProducts } from "../data/products.js";
 const Home = () => {
+  const products = getProducts();
   return (
     <div className="page">
       <div className="home-hero">
@@ -7,6 +10,21 @@ const Home = () => {
       </div>
       <div className="container">
         <h2 className="page-title">Our Products</h2>
+        <div className="product-grid">
+          {products.map((product) => (
+            <div className="product-card" key={product.id}>
+              <img src={product.image} className="product-card-image" />
+              <div className="product-card-content">
+                <h3 className="product-card-name">{product.name}</h3>
+                <p className="product-card-price">${product.price}</p>
+                <div className="product-card-actions">
+                  <Link className="btn btn-secondary">View Details</Link>
+                  <button className="btn btn-primary">Add To Cart</button>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
