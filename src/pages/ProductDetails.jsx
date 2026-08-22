66c6247f19582064951a44bd6c2853ddbfec9be4
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2026-08-22 13:07:37 -0500

    added product details page

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,5 +1,43 @@
+import { useNavigate, useParams } from "react-router-dom";
+import { getProductById } from "../data/products";
+import { useEffect, useState } from "react";
+
 const ProductDetails = () => {
-  return <div>ProductDetails</div>;
+  const [product, setProduct] = useState(null);
+  const navigate = useNavigate();
+  const { id } = useParams();
+
+  useEffect(() => {
+    const foundProduct = getProductById(id);
+
+    if (!foundProduct) {
+      navigate("/");
+      return;
+    }
+    setProduct(foundProduct);
+  }, [id]);
+
+  if (!product) {
+    return <h1>Loading...</h1>;
+  }
+
+  return (
+    <div className="page">
+      <div className="container">
+        <div className="product-detail">
+          <div className="product-detail-image">
+            <img src={product.image} alt={product.name} />
+          </div>
+          <div className="product-detail-content">
+            <h1 className="product-detail-name">{product.name}</h1>
+            <p className="product-detail-price">${product.price}</p>
+            <p className="product-detail-description">{product.description}</p>
+            <button className="btn btn-primary">Add to cart</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductDetails;
